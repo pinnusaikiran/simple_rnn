4e39ed2e643b1fd78b2e9f981c69e9ce71fcfb61
pinnusaikiran <132491371+pinnusaikiran@users.noreply.github.com>
2026-05-20 00:03:37 +0530

    Reformat code by removing and re-adding imports

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ word_index=imdb.get_word_index()
 reverse_word_index={value:key for key,value in word_index.items()}
 
 #Load the pre-trained model with ReLU activation
-model=load_model('/content/simple_rnn_imdb.keras')
+model=load_model('simple_rnn_imdb.keras')
 
 #Step2 : Helper Functions
 # Function to decode reviews
